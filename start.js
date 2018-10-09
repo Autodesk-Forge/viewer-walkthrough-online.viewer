970d74da8ea6d0570a89322d51f68641381e8779
Andrews Ang <andrews.ang@autodesk.com>
2018-10-09 13:45:38 +0800

    Removed all ForgeSDK, use URL endpoints instead.

--- start.js
+++ start.js
@@ -23,6 +23,7 @@
 var express = require('express');           // For web server
 var Axios = require('axios');               // A Promised base http client
 var bodyParser = require('body-parser');    // Receive JSON format
+var path = require('path');                 // Get file and directory pathspaths
 
 // Set up Express web server
 var app = express();
@@ -43,7 +44,7 @@ var server = app.listen(app.get('port'), function () {
 var FORGE_CLIENT_ID = '<REPLACE_WITH_FORGE_CLIENT_ID>';
 var FORGE_CLIENT_SECRET = '<REPLACE_WITH_FORGE_CLIENT_SECRET>';
 var access_token = '';
-var scopes = 'data:read';
+var scopes = 'data:read data:write data:create bucket:create bucket:read';
 const querystring = require('querystring');
 
 // // Route /auth
@@ -65,11 +66,182 @@ app.get('/auth', function (req, res) {
             // Success
             access_token = response.data.access_token;
             console.log(response);
-            res.send(access_token);
+            res.redirect('/create');
         })
         .catch(function (error) {
             // Failed
             console.log(error);
             res.send('Failed to authenticate');
         });
+});
+
+// Route /token
+app.get('/token', function (req, res) {
+    // Limit public token to Viewer read only
+    Axios({
+        method: 'POST',
+        url: 'https://developer.api.autodesk.com/authentication/v1/authenticate',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+        },
+        data: querystring.stringify({
+            client_id: FORGE_CLIENT_ID,
+            client_secret: FORGE_CLIENT_SECRET,
+            grant_type: 'client_credentials',
+            scope: 'viewables:read'
+        })
+    })
+        .then(function (response) {
+            // Success
+            console.log(response);
+            res.json({ access_token: response.data.access_token, expires_in: response.data.expires_in });
+        })
+        .catch(function (error) {
+            // Failed
+            console.log(error);
+            res.status(500).json(error);
+        });
+});
+
+const bucketKey = 'viewer_tutorial_bucket_0090128'; // Must be unique across all buckets, change last few digit
+const policyKey = 'transient'; // Expires in 24hr
+
+// Route /create
+app.get('/create', function (req, res) {
+    // Create an application shared bucket using access token from previous route
+    // We will use this bucket for storing all files in this tutorial
+    Axios({
+        method: 'POST',
+        url: 'https://developer.api.autodesk.com/oss/v2/buckets',
+        headers: {
+            'content-type': 'application/json',
+            Authorization: 'Bearer ' + access_token
+        },
+        data: JSON.stringify({
+            'bucketKey': bucketKey,
+            'policyKey': policyKey
+        })
+    })
+        .then(function (response) {
+            // Success
+            console.log(response);
+            res.redirect('/verify');
+        })
+        .catch(function (error) {
+            if (error.response && error.response.status == 409) {
+                console.log('Bucket already exists, skip creation.');
+                res.redirect('/verify');
+            }
+            // Failed
+            console.log(error);
+            res.send('Failed to create a new bucket');
+        });
+});
+
+// Route /verify
+app.get('/verify', function (req, res) {
+    Axios({
+        method: 'GET',
+        url: 'https://developer.api.autodesk.com/oss/v2/buckets/' + encodeURIComponent(bucketKey) + '/details',
+        headers: {
+            Authorization: 'Bearer ' + access_token
+        }
+    })
+        .then(function (response) {
+            // Success
+            console.log(response);
+            res.redirect('/app');
+        })
+        .catch(function (error) {
+            // Failed
+            console.log(error);
+            res.send('Failed to verify the new bucket');
+        });
+});
+
+// Route /app
+app.get('/app', function (req, res) {
+    res.sendFile(path.join(__dirname + '/www/upload.html'));
+});
+
+// For converting the source into a Base64-Encoded string
+var Buffer = require('buffer').Buffer;
+String.prototype.toBase64 = function () {
+    // Buffer is part of Node.js to enable interaction with octet streams in TCP streams, 
+    // file system operations, and other contexts.
+    return new Buffer(this).toString('base64');
+};
+
+var multer = require('multer');         // To handle file upload
+var upload = multer({ dest: 'tmp/' }); // Save file into local /tmp folder
+
+// Route /upload
+app.post('/upload', upload.single('fileToUpload'), function (req, res) {
+    var fs = require('fs'); // Node.js File system for reading files
+    fs.readFile(req.file.path, function (err, filecontent) {
+        Axios({
+            method: 'PUT',
+            url: 'https://developer.api.autodesk.com/oss/v2/buckets/' + encodeURIComponent(bucketKey) + '/objects/' + encodeURIComponent(req.file.originalname),
+            headers: {
+                Authorization: 'Bearer ' + access_token,
+                'Content-Disposition': req.file.originalname,
+                'Content-Length': filecontent.length
+            },
+            data: filecontent
+        })
+            .then(function (response) {
+                // Success
+                console.log(response);
+                var urn = response.data.objectId.toBase64();
+                res.redirect('/translate/' + urn);
+            })
+            .catch(function (error) {
+                // Failed
+                console.log(error);
+                res.send('Failed to create a new object in the bucket');
+            });
+    });
+});
+
+// Route /translate
+app.get('/translate/:urn', function (req, res) {
+    var urn = req.params.urn;
+    var format_type = 'svf';
+    var format_views = ['2d', '3d'];
+    Axios({
+        method: 'POST',
+        url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/job',
+        headers: {
+            'content-type': 'application/json',
+            Authorization: 'Bearer ' + access_token
+        },
+        data: JSON.stringify({
+            'input': {
+                'urn': urn
+            },
+            'output': {
+                'formats': [
+                    {
+                        'type': format_type,
+                        'views': format_views
+                    }
+                ]
+            }
+        })
+    })
+        .then(function (response) {
+            // Success
+            console.log(response);
+            res.redirect('/viewer?urn=' + urn);
+        })
+        .catch(function (error) {
+            // Failed
+            console.log(error);
+            res.send('Error at Model Derivative job.');
+        });
+});
+
+// Route /viewer
+app.get('/viewer', function (req, res) {
+    res.sendFile(path.join(__dirname + '/www/viewer.html'));
 });
