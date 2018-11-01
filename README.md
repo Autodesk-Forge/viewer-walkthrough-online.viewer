f95954740780e62fdb2bb9721d0945b506dfc36d
Andrews Ang <andrews.ang@autodesk.com>
2018-11-01 14:55:22 +0800

    Moved Client ID and Secret out to env variables, changed port to 3000, changed path to match FPD standards, use HTML directly instead of using path.

--- README.md
+++ README.md
@@ -1,34 +1,81 @@
-# Two-Legged Authentication sample
+# Online Viewer sample
 
 [![Node.js](https://img.shields.io/badge/Node.js-8.11.1-blue.svg)](https://nodejs.org/)
 [![npm](https://img.shields.io/badge/npm-6.1.0-blue.svg)](https://www.npmjs.com/)
+![Platforms](https://img.shields.io/badge/platform-windows%20%7C%20osx%20%7C%20linux-lightgray.svg)
 [![License](http://img.shields.io/:license-mit-blue.svg)](http://opensource.org/licenses/MIT)
 
 [![oAuth2](https://img.shields.io/badge/oAuth2-v1-green.svg)](https://forge.autodesk.com/)
+[![Data-Management](https://img.shields.io/badge/Data%20Management-v2-green.svg)](https://forge.autodesk.com/)
+[![Model-Derivative](https://img.shields.io/badge/Model%20Derivative-v2-green.svg)](https://forge.autodesk.com/)
+[![Viewer](https://img.shields.io/badge/Viewer-v6-green.svg)](https://forge.autodesk.com/)
 
-## Description
-This sample illustrates the Two-Legged Authentication workflow:
-- Run a simple web server
-- Create a Two-Legged Authentication access token to be used throughout the app
+# Description
+This sample is part of the [Online Viewer Walkthrough](https://forge.autodesk.com/developer/learn/viewer-app).
 
-## Setup & Run
-1. Setup
-   - Install `npm` (if you haven't already).
-   - Install `npm` packages using the command `npm install`.
-     - Make sure that the current directory contains `package.json`.
-   - Ensure that in your hosts file you have mapped:
-     - `127.0.0.1 localhost`
-   - Replace the `FORGE_CLIENT_ID` and `FORGE_CLIENT_SECRET` variable values in `start.js` with your Forge credentials.
+In this sample, we will be creating a simple web page that allows end users to upload some files to Forge storage service, then translate them and display them in a web browser.
 
-2. Execute `node start.js`
-   - The node server will listen on on port `5000`.
+- Authenticate your developer identity
+- Create a bucket
+- Get bucket detail
+- Save a supported file to a bucket
+- Read the uploaded file from a bucket
+- Translate the file to the SVF format
+- Display the file in a web browser
 
-3. Open a browser and navigate to `http://localhost:5000/`.
-
-4. Click on the `Authorize me` link and login to your Autodesk account.
-   - In your console, you should see `Server listening on port 5000`.
-   - In the browser, you should see a token string.
-   - That's it!
-
-### Thumbnail
+## Thumbnail
 ![thumbnail](/thumbnail.png)
+
+# Setup
+
+## Prerequisites
+
+1. A Forge account: [Getting Started with Forge](https://forge.autodesk.com/developer/getting-started)
+2. A text editor (We recommend http://brackets.io/ or https://atom.io/, it's up to you.)
+3. A basic knowledge of :
+    - HTML and CSS
+    - JavaScript ES6
+    - Command-line programs
+      - Node.js Command Line (for Windows users)
+      - Terminal (for Mac/Linux/Unix users)
+
+## Running locally
+
+Install [NodeJS](https://nodejs.org/) (version 8 or newer).
+
+Clone this project or download it. It's recommended to install [GitHub desktop](https://desktop.github.com/).
+
+To run it, install the required packages, set the enviroment variables with your client ID & secret and finally start it. Via command line, navigate to the folder where this repository was cloned and use the following:
+
+Mac OSX/Linux (Terminal)
+
+```bash
+npm install
+export FORGE_CLIENT_ID=<<YOUR CLIENT ID FROM DEVELOPER PORTAL>>
+export FORGE_CLIENT_SECRET=<<YOUR CLIENT SECRET>>
+npm start
+```
+
+Windows (use Node.js command line from Start menu)
+
+```bash
+npm install
+set FORGE_CLIENT_ID=<<YOUR CLIENT ID FROM DEVELOPER PORTAL>>
+set FORGE_CLIENT_SECRET=<<YOUR CLIENT SECRET>>
+npm start
+```
+
+Open a browser and navigate to http://localhost:3000.
+
+Click on the `Authorize me` link and then click through the links on the browser to:
+  - Create a bucket
+  - Save a supported file to a bucket
+  - Display the file in a web browser
+
+*NOTE:* _It may take several minutes to complete translate the file._
+
+# License
+This sample is licensed under the terms of the [MIT License](http://opensource.org/licenses/MIT). Please see the [LICENSE](LICENSE) file for full details.
+
+# Support
+forge.help@autodesk.com
--- start.js
+++ start.js
@@ -23,32 +23,31 @@
 var express = require('express');           // For web server
 var Axios = require('axios');               // A Promised base http client
 var bodyParser = require('body-parser');    // Receive JSON format
-var path = require('path');                 // Get file and directory pathspaths
 
 // Set up Express web server
 var app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/www'));
 
-// This is for web server to start listening to port 5000
-app.set('port', 5000);
+// This is for web server to start listening to port 3000
+app.set('port', 3000);
 var server = app.listen(app.get('port'), function () {
     console.log('Server listening on port ' + server.address().port);
 });
 
 //-------------------------------------------------------------------
 // Configuration for your Forge account
-// Initialize the 2-legged OAuth2 client, set specific scopes and
-// set the token to auto refresh
+// Initialize the 2-legged OAuth2 client, and
+// set specific scopes
 //-------------------------------------------------------------------
-var FORGE_CLIENT_ID = '<REPLACE_WITH_FORGE_CLIENT_ID>';
-var FORGE_CLIENT_SECRET = '<REPLACE_WITH_FORGE_CLIENT_SECRET>';
+var FORGE_CLIENT_ID = process.env.FORGE_CLIENT_ID;
+var FORGE_CLIENT_SECRET = process.env.FORGE_CLIENT_SECRET;
 var access_token = '';
 var scopes = 'data:read data:write data:create bucket:create bucket:read';
 const querystring = require('querystring');
 
-// // Route /auth
-app.get('/auth', function (req, res) {
+// // Route /api/forge/oauth
+app.get('/api/forge/oauth', function (req, res) {
     Axios({
         method: 'POST',
         url: 'https://developer.api.autodesk.com/authentication/v1/authenticate',
@@ -66,7 +65,7 @@ app.get('/auth', function (req, res) {
             // Success
             access_token = response.data.access_token;
             console.log(response);
-            res.redirect('/create');
+            res.redirect('/api/forge/datamanagement/bucket/create');
         })
         .catch(function (error) {
             // Failed
@@ -75,8 +74,8 @@ app.get('/auth', function (req, res) {
         });
 });
 
-// Route /token
-app.get('/token', function (req, res) {
+// Route /api/forge/oauth/public
+app.get('/api/forge/oauth/public', function (req, res) {
     // Limit public token to Viewer read only
     Axios({
         method: 'POST',
@@ -103,11 +102,11 @@ app.get('/token', function (req, res) {
         });
 });
 
-const bucketKey = 'viewer_tutorial_bucket_0090128'; // Must be unique across all buckets, change last few digit
+const bucketKey = FORGE_CLIENT_ID.toLowerCase() + '_tutorial_bucket'; // Prefix with your ID so the bucket key is unique across all buckets on all other accounts
 const policyKey = 'transient'; // Expires in 24hr
 
-// Route /create
-app.get('/create', function (req, res) {
+// Route /api/forge/datamanagement/bucket/create
+app.get('/api/forge/datamanagement/bucket/create', function (req, res) {
     // Create an application shared bucket using access token from previous route
     // We will use this bucket for storing all files in this tutorial
     Axios({
@@ -125,12 +124,12 @@ app.get('/create', function (req, res) {
         .then(function (response) {
             // Success
             console.log(response);
-            res.redirect('/verify');
+            res.redirect('/api/forge/datamanagement/bucket/detail');
         })
         .catch(function (error) {
             if (error.response && error.response.status == 409) {
                 console.log('Bucket already exists, skip creation.');
-                res.redirect('/verify');
+                res.redirect('/api/forge/datamanagement/bucket/detail');
             }
             // Failed
             console.log(error);
@@ -138,8 +137,8 @@ app.get('/create', function (req, res) {
         });
 });
 
-// Route /verify
-app.get('/verify', function (req, res) {
+// Route /api/forge/datamanagement/bucket/detail
+app.get('/api/forge/datamanagement/bucket/detail', function (req, res) {
     Axios({
         method: 'GET',
         url: 'https://developer.api.autodesk.com/oss/v2/buckets/' + encodeURIComponent(bucketKey) + '/details',
@@ -150,18 +149,13 @@ app.get('/verify', function (req, res) {
         .then(function (response) {
             // Success
             console.log(response);
-            res.redirect('/app');
+            res.redirect('/upload.html');
         })
         .catch(function (error) {
             // Failed
             console.log(error);
             res.send('Failed to verify the new bucket');
         });
-});
-
-// Route /app
-app.get('/app', function (req, res) {
-    res.sendFile(path.join(__dirname + '/www/upload.html'));
 });
 
 // For converting the source into a Base64-Encoded string
@@ -175,8 +169,8 @@ String.prototype.toBase64 = function () {
 var multer = require('multer');         // To handle file upload
 var upload = multer({ dest: 'tmp/' }); // Save file into local /tmp folder
 
-// Route /upload
-app.post('/upload', upload.single('fileToUpload'), function (req, res) {
+// Route /api/forge/datamanagement/bucket/upload
+app.post('/api/forge/datamanagement/bucket/upload', upload.single('fileToUpload'), function (req, res) {
     var fs = require('fs'); // Node.js File system for reading files
     fs.readFile(req.file.path, function (err, filecontent) {
         Axios({
@@ -193,7 +187,7 @@ app.post('/upload', upload.single('fileToUpload'), function (req, res) {
                 // Success
                 console.log(response);
                 var urn = response.data.objectId.toBase64();
-                res.redirect('/translate/' + urn);
+                res.redirect('/api/forge/modelderivative/' + urn);
             })
             .catch(function (error) {
                 // Failed
@@ -203,8 +197,8 @@ app.post('/upload', upload.single('fileToUpload'), function (req, res) {
     });
 });
 
-// Route /translate
-app.get('/translate/:urn', function (req, res) {
+// Route /api/forge/modelderivative
+app.get('/api/forge/modelderivative/:urn', function (req, res) {
     var urn = req.params.urn;
     var format_type = 'svf';
     var format_views = ['2d', '3d'];
@@ -232,16 +226,11 @@ app.get('/translate/:urn', function (req, res) {
         .then(function (response) {
             // Success
             console.log(response);
-            res.redirect('/viewer?urn=' + urn);
+            res.redirect('/viewer.html?urn=' + urn);
         })
         .catch(function (error) {
             // Failed
             console.log(error);
             res.send('Error at Model Derivative job.');
         });
-});
-
-// Route /viewer
-app.get('/viewer', function (req, res) {
-    res.sendFile(path.join(__dirname + '/www/viewer.html'));
 });
